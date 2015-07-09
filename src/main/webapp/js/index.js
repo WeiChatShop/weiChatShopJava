6de74745cdf03917653a478c096b926253b8b1c7
zmwxiaoming <563652043@qq.com>
2015-07-09 13:14:38 +0800

    轮播图搞了

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -12,11 +12,11 @@ function buildHtml(){
             if(bookList==null){
                 $("#bookshow").append("<span class='glyphicon glyphicon-fire' style='color: rgb(255, 140, 60);'>Sorry ,新书正在火速赶来,敬请期待!</span>")
             }else {
+                var com = 0;
+                var art=0;
                 for(var i=0;i<bookList.length; i++ ){
-                    var com = 0;
-                    var art=0;
                     if(bookList[i].classify_id==1){
-                        if(i==0){
+                        if(com==0){
                             $("#computerLi").append('<li data-target="#computerBook" data-slide-to="0" class="active"></li>');
                             $("#computerItem").append('<div class="active item"><img src="'+bookList[i].path+'"><div class="carousel-caption">' +
                             '<h4>'+bookList[i].name+'</h4></div></div>');
@@ -26,8 +26,8 @@ function buildHtml(){
                             '<h4>'+bookList[i].name+'</h4></div></div>');
                         }
                         com++;
-                    }else {
-                        if(i==0) {
+                    }else if(bookList[i].classify_id==2){
+                        if(art==0) {
                             $("#artBookLi").append('<li data-target="#artBook" data-slide-to="0" class="active"></li>');
                             $("#artBookItem").append('<div class="active item"><img src="'+bookList[i].path+'"><div class="carousel-caption">' +
                             '<h4>'+bookList[i].name+'</h4></div></div>');
@@ -41,10 +41,10 @@ function buildHtml(){
                 }
             }
             $('#computerBook').carousel({
-                interval: 4000
+                interval: 3000
             })
             $('#artBook').carousel({
-                interval: 4000
+                interval: 2000
             })
         }
     });
